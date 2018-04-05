e882d9ecf922285f5bc347a69a9d009bc968f75f
edwardsanchez001 <jesus.ed.sanchez@gmail.com>
2018-04-05 15:25:09 -0500

    added some changes to the selections in reservations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,8 @@ class Customer < ApplicationRecord
   # belongs_to :customer_status
 
   has_many :reservations
+
+  def full_customer_name
+      ' ' + self.FirstName + ' ' + self.LastName
+  end
 end
