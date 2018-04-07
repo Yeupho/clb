738e430687cacf2cd3606718890bf7081aee2b25
edwardsanchez001 <jesus.ed.sanchez@gmail.com>
2018-04-06 21:26:17 -0500

    second bottle

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -31,7 +31,7 @@ class ReservationsController < ApplicationController
     respond_to do |format|
       if @reservation.save
         format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
-        format.json { render :show, status: :created, location: @reservation }
+        format.json { render :index, status: :created, location: @reservation }
       else
         format.html { render :new }
         format.json { render json: @reservation.errors, status: :unprocessable_entity }
